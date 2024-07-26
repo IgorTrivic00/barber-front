399a701f48e5c77de7d4e6e4b2fe5681193195e5
dusans <dusan.stojmenovic@plusplusnt.rs>
2024-07-26 13:36:46 +0200

    [develop] Uradjena home stranica bez mape treba dodati deep-link

--- src/app/main/components/home/home.component.ts
+++ src/app/main/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {Button} from "primeng/button";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    Button
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
