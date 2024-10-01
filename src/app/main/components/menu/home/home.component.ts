803bb8bd0fa7ed62796d62f2104e0d5fda1cacd2
“IgorTrivic00” <trivicigor00@gmail.com>
2024-10-01 16:08:01 +0200

    instagram problem i sredjivanje wrappera

--- src/app/main/components/menu/home/home.component.ts
+++ src/app/main/components/menu/home/home.component.ts
@@ -16,8 +16,31 @@ import {WorkDaySwiperComponent} from "./work-day-swiper/work-day-swiper.componen
 export class HomeComponent {
 
   redirectToInstagram() {
-    const url = 'instagram://user?username=' + enviroment.instagramUserName;
-    window.location.href = url;
+
+    // ovo je obrisan deo ne treba ti
+   /* const url = 'instagram://user?username=' + enviroment.instagramUserName;
+    window.location.href = url;*/
+
+    const instagramUserName = enviroment.instagramUserName;
+  
+    // Prov radimo proveru da li je korisnik na mobilnom uredjaju ili nije 
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  
+    if (isMobile) {
+      // AAko jeste onda pokusava da dje na aplikaciju ako je imas instaliranu ako ne ,
+      const appUrl = `instagram://user?username=${instagramUserName}`;
+      window.location.href = appUrl;
+  
+      //  otvorice ti  web verziju
+      setTimeout(() => {
+        const webUrl = `https://www.instagram.com/${instagramUserName}/`;
+        window.location.href = webUrl;
+      }, 500); 
+    } else {
+      // Ako nije mobilni uređaj, preusmeri direktno na web verziju
+      const webUrl = `https://www.instagram.com/${instagramUserName}/`;
+      window.location.href = webUrl;
+    }
   }
 
   redirectToCall() {
